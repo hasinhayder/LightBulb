7136379519befff89dc6160760260ea50f97a1b9
hasinhayder <ironhide@leevio.com>
2011-10-03 20:40:39 +0900

    Started working on Photos

--- js/lb.photos.js
+++ js/lb.photos.js
@@ -3,5 +3,10 @@
  */
 
 $.fn.LightBulb.photos = {
-    
+    createAlbum:function(user,creator, name,description, location,privacy, type){
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        if(!type) type="normal";
+        if(!privacy) privacy="everyone";
+    }
 }
