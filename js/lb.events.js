a0a34188b96e7b1c100c32b47a395aa46ff164e8
hasinhayder <ironhide@leevio.com>
2011-10-03 18:41:18 +0900

    Added All Event Functions

--- js/lb.events.js
+++ js/lb.events.js
@@ -32,32 +32,138 @@ $.fn.LightBulb.events = {
                 if(jQuery.isFunction(opts.callback)) opts.callback(response);
             })
         }else{
-            //throw exception
+            throw "User must be logged in";
         }
 
     },
-    update:function(eventId, name, location, startTime, endTime, description) {
-
+    update:function(eventId, name, location, startTime, endTime, description, privacy) {
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        if (accessToken) {
+            var eventData = {
+                "access_token": accessToken,
+                "start_time" : startTime,
+                "end_time":endTime,
+                "location" : location,
+                "name" : name,
+                "description":description,
+                "privacy":privacy
+            }
+            FB.api("/"+eventId+"", 'post', eventData, function(response) {
+                if(jQuery.isFunction(opts.callback)) opts.callback(response);
+            })
+        }else{
+            throw "User must be logged in";
+        }
     },
     updateName:function(eventId, name) {
-
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        if (accessToken) {
+            var eventData = {
+                "access_token": accessToken,
+                "name" : name
+            }
+            FB.api("/"+eventId+"", 'post', eventData, function(response) {
+                if(jQuery.isFunction(opts.callback)) opts.callback(response);
+            })
+        }else{
+            throw "User must be logged in";
+        }
     },
     updateLocation:function(eventId, location) {
-
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        if (accessToken) {
+            var eventData = {
+                "access_token": accessToken,
+                "location" : location
+            }
+            FB.api("/"+eventId+"", 'post', eventData, function(response) {
+                if(jQuery.isFunction(opts.callback)) opts.callback(response);
+            })
+        }else{
+            throw "User must be logged in";
+        }
     },
     updateTime:function(eventId, startTime, endTime) {
-
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        if (accessToken) {
+            var eventData = {
+                "access_token": accessToken,
+                "start_time" : startTime,
+                "end_time":endTime
+            }
+            FB.api("/"+eventId+"", 'post', eventData, function(response) {
+                if(jQuery.isFunction(opts.callback)) opts.callback(response);
+            })
+        }else{
+            throw "User must be logged in";
+        }
     },
     updateDescription:function(eventId, description) {
-
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        if (accessToken) {
+            var eventData = {
+                "access_token": accessToken,
+                "description" : description
+            }
+            FB.api("/"+eventId+"", 'post', eventData, function(response) {
+                if(jQuery.isFunction(opts.callback)) opts.callback(response);
+            })
+        }else{
+            throw "User must be logged in";
+        }
     },
     delete:function(eventId) {
-
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        if (accessToken) {
+            var eventData = {
+                "access_token": accessToken
+            }
+            FB.api("/"+eventId+"", 'delete', eventData, function(response) {
+                if(jQuery.isFunction(opts.callback)) opts.callback(response);
+            })
+        }else{
+            throw "User must be logged in";
+        }
     },
     invite:function(eventId, friends) {
-
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        if (accessToken) {
+            var eventData = {
+                "access_token": accessToken,
+                "users":friends.join(",")
+            }
+            FB.api("/"+eventId+"/invite", 'post', eventData, function(response) {
+                if(jQuery.isFunction(opts.callback)) opts.callback(response);
+            })
+        }else{
+            throw "User must be logged in";
+        }
     },
-    updateRSVP:function(eventId, friends, rsvp) {
-
+    /**
+     *
+     * @param eventId
+     * @param friend Facebook User Id
+     * @param rsvp String mentioning the users RSVP status, either "attending","maybe","declined" or "noreply'
+     */
+    updateRSVP:function(eventId, friend, rsvp) {
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        if (accessToken) {
+            var eventData = {
+                "access_token": accessTokens
+            }
+            FB.api("/"+eventId+"/"+rsvp+"/"+friend, 'post', eventData, function(response) {
+                if(jQuery.isFunction(opts.callback)) opts.callback(response);
+            })
+        }else{
+            throw "User must be logged in";
+        }
     }
 };
