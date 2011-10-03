770c376b005bc5ed6535044006ef0208a4de39ec
hasinhayder <ironhide@leevio.com>
2011-10-03 18:21:07 +0900

    Creating a Facebook Event

--- js/lb.events.js
+++ js/lb.events.js
@@ -3,6 +3,18 @@
  */
 
 $.fn.LightBulb.events = {
+
+    /**
+     * @author Hasin Hayder
+     * @param user Facebook User Id, either string "me" or a particular Facebook User/Page Id
+     * @param name Name of the event
+     * @param location Location of the event
+     * @param startTime Start time of the event in either unix timestamp or in m/d/Y H:i format
+     * @param endTime End time of the event in either unix timestamp or in m/d/Y H:i format
+     * @param description Destiption of the event
+     * @param privacy Privacy of the event
+     * @param callback Callback function, which will be invoked when the event creation is successful or unsuccessful
+     */
     create: function(user, name, location, startTime, endTime, description, privacy, callback) {
         var userData = $.fn.LightBulb._getFacebookData();
         var accessToken = userData.accessToken;
