61ac6c58980ca2c9a90ceb1230417dd01cf9dee2
hasinhayder <ironhide@leevio.com>
2011-10-03 17:28:21 +0900

    Empty shell for Events Api

---
+++ js/lb.events.js
@@ -0,0 +1,33 @@
+/**
+ * Helper functions to manage Facebook events using LightBulb.
+ */
+
+$.fn.LightBulb.events = {
+    create: function(name,location, startTime, endTime, description){
+        
+    },
+    update:function(eventId, name,location, startTime, endTime, description){
+
+    },
+    updateName:function(eventId, name){
+        
+    },
+    updateLocation:function(eventId, location){
+
+    },
+    updateTime:function(eventId, startTime,endTime){
+
+    },
+    updateDescription:function(eventId,description){
+
+    },
+    delete:function(eventId){
+
+    },
+    invite:function(eventId, friends){
+
+    },
+    updateRSVP:function(eventId, friends, rsvp){
+
+    }
+};
