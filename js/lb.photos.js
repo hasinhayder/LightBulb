82e8ced42aa257769c52433039c13b73b834fab7
hasinhayder <ironhide@leevio.com>
2011-10-03 22:47:54 +0900

    Working on retrieving meta information of an album id (everything without the photos)

--- js/lb.photos.js
+++ js/lb.photos.js
@@ -6,12 +6,58 @@ $.fn.LightBulb.photos = {
     createAlbum:function(user,creator, name,description, location,privacy, type){
         var userData = $.fn.LightBulb._getFacebookData();
         var accessToken = userData.accessToken;
+        var userId = userdata.facebookUserId;
+        if(creator=="") creator = userId;
         if(!type) type="normal";
         if(!privacy) privacy="everyone";
         if(accessToken){
             //create the album
-        }else{
-            throw "User must be logged in";
+            var eventData = {
+                "access_token": accessToken,
+                "from" : creator,
+                "name":name,
+                "description" : description,
+                "location" : location,
+                "type":type,
+                "privacy":privacy
+            }
+            FB.api("/"+user+"/albums", 'post', eventData, function(response) {
+                if(jQuery.isFunction(callback)) callback.call(this, response);
+            })
+        } else {
+            throw LIGHTBULB_NO_TOKEN;
+        }
+    },
+    get:function(albumId){
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        var userId = userdata.facebookUserId;
+        if(accessToken){
+            var eventData = {
+                "access_token": accessToken
+            }
+            FB.api("/"+albumId, 'post', eventData, function(response) {
+                if(jQuery.isFunction(callback)) callback.call(this, response);
+            })
+        }
+        else{
+            throw LIGHTBULB_NO_TOKEN;
+        }
+    },
+    getMeta:function(albumId){
+        var userData = $.fn.LightBulb._getFacebookData();
+        var accessToken = userData.accessToken;
+        var userId = userdata.facebookUserId;
+        if(accessToken){
+            var eventData = {
+                "access_token": accessToken
+            }
+            FB.api("/"+albumId, 'post', eventData, function(response) {
+                if(jQuery.isFunction(callback)) callback.call(this, response);
+            })
+        }
+        else{
+            throw LIGHTBULB_NO_TOKEN;
         }
     }
 }
