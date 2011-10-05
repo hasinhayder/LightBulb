73017e4b9e2b4a192e79070bca632cb27f2f7e17
hasinhayder <ironhide@leevio.com>
2011-10-05 16:36:49 +0900

    Publish stream dialog is done

--- js/lb.dialogs.js
+++ js/lb.dialogs.js
@@ -3,10 +3,39 @@
  */
 (function() {
     LightBulb.dialogs = {
-        publishFeed:function(to,from,name,caption, link, description,picture,video,actionName, actionLink){
-            
+        publishFeed:function(to, from, name, caption, link, description, picture, video, actionName, actionLink, callback) {
+            var userData = LightBulb._getFacebookData();
+            var accessToken = userData.accessToken;
+            if (accessToken) {
+                var action = {}
+                if (to == "") to = "me";
+                if (from == "") from = "me"
+                if (actionName != "" && actionLink != "") {
+                    action = {
+                        name:actionName,
+                        link:actionLink
+                    }
+                }
+                var data = {
+                    method: 'feed',
+                    link: link,
+                    picture: picture,
+                    name: name,
+                    caption: caption,
+                    description: description,
+                    to:to,
+                    from:from,
+                    source: video,
+                    actions:action
+                }
+                FB.ui(data, function(response) {
+                    if (jQuery.isFunction(callback)) callback.call(this, response);
+                });
+            }else{
+                throw LIGHTBULB_NO_TOKEN;
+            }
         },
-        publishFeedBackground:function(){
+        publishFeedBackground:function() {
 
         }
     }
