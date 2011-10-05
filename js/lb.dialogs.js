c88feec38025c018f50520902bb437c0d94e5a47
hasinhayder <ironhide@leevio.com>
2011-10-05 16:25:47 +0900

    Started working on lb.dialog.js to automate user interaction

---
+++ js/lb.dialogs.js
@@ -0,0 +1,13 @@
+/**
+ * Wrap all methods from Fb.ui of the JS-SDK to automate interacting with users
+ */
+(function() {
+    LightBulb.dialogs = {
+        publishFeed:function(to,from,name,caption, link, description,picture,video,actionName, actionLink){
+            
+        },
+        publishFeedBackground:function(){
+
+        }
+    }
+})();
