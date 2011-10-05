b552a9d7459d36c1b3e7c5a739a7fdac8b787e46
hasinhayder <ironhide@leevio.com>
2011-10-05 17:53:04 +0900

    Publish feed in BackGround is done - no user interaction required

--- js/lb.dialogs.js
+++ js/lb.dialogs.js
@@ -5,8 +5,8 @@
 (function() {
     LightBulb.dialogs = {
         /**
-         * @author Hasin Hayder
          * Prompt a publish feed dialog to the currently logged in user
+         * @author Hasin Hayder
          * @param to Facebook User/Page id on whos wall this feed is going to publish
          * @param from Facebook User/Page id - the creator of the feed
          * @param name Name of the feed
@@ -52,8 +52,54 @@
                 throw LIGHTBULB_NO_TOKEN;
             }
         },
-        publishFeedBackground:function() {
 
+        /**
+         * Publish feed directly, without any user interaction
+         * @author Hasin Hayder
+         * @param to Facebook User/Page id on whos wall this feed is going to publish
+         * @param from Facebook User/Page id - the creator of the feed
+         * @param name Name of the feed
+         * @param caption Caption of the feed
+         * @param link Link of the feed
+         * @param description Description of the feed
+         * @param picture http based URL of the external picture
+         * @param video  http based URL of swf or flv files
+         * @param actionName Name of the action, optional
+         * @param actionLink Link of the action, optional
+         * @param callback
+         */
+        publishFeedBackground:function(to, from, name, caption, link, description, picture, video, actionName, actionLink, callback) {
+            var userData = LightBulb._getFacebookData();
+            var accessToken = userData.accessToken;
+            var userId = userData.facebookUserId;
+            if (accessToken) {
+                var action = "";
+                if (to == "") to = userId;
+                if (from == "") from = userId
+                if (actionName != "" && actionLink != "") {
+                    action = [{
+                        name:actionName,
+                        link:actionLink
+                    }]
+                }
+                var data = {
+                    method: 'feed',
+                    link: link,
+                    picture: picture,
+                    name: name,
+                    caption: caption,
+                    description: description,
+                    to:to,
+                    from:from,
+                    source: video,
+                    actions:action
+                }
+                FB.api("/" + to + "/feed", 'post', data, function(response) {
+                    if (jQuery.isFunction(callback)) callback.call(this, response);
+                })
+            }else{
+                throw LIGHTBULB_NO_TOKEN;
+            }
         }
     }
 })();
