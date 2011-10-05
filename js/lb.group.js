8c458f71978a4622f8a046207a1304332e297b48
BIJON <bijon@BIJONs-MacBook-Pro.local>
2011-10-05 18:25:18 +0600

    add group wrapper

---
+++ js/lb.group.js
@@ -0,0 +1,59 @@
+/**
+ *@author saidur rahman
+ * Graph API to manage a Facebook group
+ */
+//CREATE A LINK, POST STATUS, MESSAGE in a group  
+
+/*var LIGHTBULB_ALBUM_PRIVACY_EVERYONE = "EVERYONE";
+var LIGHTBULB_ALBUM_PRIVACY_ALLFRIENDS = "ALL_FRIENDS";
+var LIGHTBULB_ALBUM_PRIVACY_NETWORKS_FRIENDS = "NETWORKS_FRIENDS";
+var LIGHTBULB_ALBUM_PRIVACY_FRIENDS_FRIENDS = "FRIENDS_OF_FRIENDS";
+*/
+
+(function() {
+    LightBulb.group = {
+        
+        createStatusesPosts : function (groupId,message,callback)
+        {
+            var userData = $.LightBulb._getFacebookData();
+            var accessToken = userData.accessToken;
+            
+            var userId = userData.facebookUserId;
+            if (accessToken) {
+                var eventData = {
+                    "access_token": accessToken,
+                    "message":message
+                }
+                FB.api("/" + groupId + "/feed", 'post', eventData, function(response) {
+                    if (jQuery.isFunction(callback)) callback.call(this, response);
+                    //console.log(response);
+                   
+                });
+            } else {
+                throw LIGHTBULB_NO_TOKEN;
+            }
+          
+        },
+        
+        
+
+        get:function(groupId, callback) {
+            var userData = $.LightBulb._getFacebookData();
+            var accessToken = userData.accessToken;
+            var userId = userData.facebookUserId;
+            if (accessToken) {
+                var eventData = {
+                    "access_token": accessToken
+                }
+            } else {
+                eventData = {};
+            }
+            FB.api("/" + albumId, 'post', eventData, function(response) {
+                if (jQuery.isFunction(callback)) callback.call(this, response);
+            });
+        }
+       
+       
+    }
+
+})();
