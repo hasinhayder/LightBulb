6e99939dad8e1112f6bb503d883360bde0e4f896
hasinhayder <ironhide@leevio.com>
2011-10-10 14:25:06 +0900

    Photos Wrapper complete

--- js/lb.notes.js
+++ js/lb.notes.js
@@ -6,6 +6,13 @@
 (function(){
     LightBulb.notes = {
         currentToken:"",
+        /**
+         * Return notes from a particular user or a page or a group
+         *
+         * @author Hasin Hayder
+         * @param userId Facebook user id or page id or group id
+         * @param callback
+         */
         getNotes:function(userId,callback){
             var accessToken = LightBulb.notes.getToken();
             if (accessToken) {
@@ -19,6 +26,26 @@
                 throw LIGHTBULB_NO_TOKEN;
             }
         },
+        getNote:function(noteId,callback){
+            var accessToken = LightBulb.notes.getToken();
+            if (accessToken) {
+                var data = {
+                    "access_token": accessToken
+                }
+                FB.api("/" + noteId , 'get', data, function(response) {
+                    if (jQuery.isFunction(callback)) callback.call(this, response);
+                });
+            } else {
+                throw LIGHTBULB_NO_TOKEN;
+            }
+        },
+
+        /**
+         * User must set the page token to retrieve notes from page. User lb.pages.js to get Page specific tokens
+         *
+         * @author Hasin Hayder
+         * @param token
+         */
         setToken:function(token){
             LightBulb.notes.currentToken = token;
             return true;
@@ -26,12 +53,28 @@
         removeToken:function(){
             LightBulb.notes.currentToken = "";
         },
+
+        /**
+         * Return currently active token, either of a user or a page
+         *
+         * @author Hasin Hayder
+         */
         getToken:function(){
             var userData = LightBulb._getFacebookData();
             var token = userData.accessToken;
             if(LightBulb.notes.currentToken) token=LightBulb.notes.currentToken;
             return token;
         },
+
+        /**
+         * Post a note to a user's wall, pages wall or a groups wall
+         *
+         * @author Hasin Hayder
+         * @param userId
+         * @param subject
+         * @param message
+         * @param callback
+         */
         createNote:function(userId, subject,message,callback){
             var accessToken = LightBulb.notes.getToken();
             if (accessToken) {
