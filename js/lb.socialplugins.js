01a9356025bb73c56b1a62342443f4e4a142175e
M A Hossain Tonu <mahtonu@gmail.com>
2011-11-03 23:25:40 +0600

    social plugins wrapper lib added; methods skeleton created

---
+++ js/lb.socialplugins.js
@@ -0,0 +1,23 @@
+/**
+ * Wrapper for social plugins
+ *
+ * @author M A Hossain Tonu
+ */
+(function(){
+    LightBulb.socialplugins = {
+
+        getLikeButton:function(){
+            
+        },
+
+        getSendButton: function(){},
+        getComments: function(){},
+        getActivityFeeds: function(){},
+        getRecommendations: function(){},
+        getLikeBox: function(){},
+        getLoginButton: function(){},
+        getRegistration: function(){},
+        getFacepile: function(){},
+        getLiveStream: function(){}
+    }
+})();
