ea38d8404ee3ac52d5a3b1cc8765580e7b5bf0b5
hasinhayder <ironhide@leevio.com>
2012-01-30 18:03:54 +0600

    runFQL utility method

--- js/lb.utility.js
+++ js/lb.utility.js
@@ -1,8 +1,51 @@
 /*
  * Utility functions which cannot be categorized in any other category
  */
-(function($) {
+(function ($) {
     LightBulb.utility = {
-        
+        currentToken:"",
+        /**
+         * User must set the page token to retrieve utility from page. User lb.pages.js to get Page specific tokens
+         *
+         * @author Hasin Hayder
+         * @param token
+         */
+        setToken:function (token) {
+            LightBulb.utility.currentToken = token;
+            return true;
+        },
+        removeToken:function () {
+            LightBulb.utility.currentToken = "";
+        },
+
+        /**
+         * Return currently active token, either of a user or a page
+         *
+         * @author Hasin Hayder
+         */
+        getToken:function () {
+            var userData = LightBulb._getFacebookData();
+            var token = userData.accessToken;
+            if (LightBulb.utility.currentToken) token = LightBulb.utility.currentToken;
+            return token;
+        },
+        runFQL:function (parameters) {
+            var defaults = {
+                fql:""
+            };
+            var params = $.extend(defaults, parameters);
+            var accessToken = LightBulb.utility.getToken();
+            if (accessToken && params.fql) {
+                var data = {
+                    access_token:accessToken,
+                    q:params.fql
+                };
+                FB.api("/fql" , "get", data, function (response) {
+                    if (jQuery.isFunction(callback)) callback.call(this, response);
+                });
+            } else {
+                throw LIGHTBULB_NO_TOKEN;
+            }
+        }
     };
 })(jQuery);
